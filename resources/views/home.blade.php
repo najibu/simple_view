493164064d67e775eff0ae13db1b1eecb3b6582a
najibu <ns.najibu@gmail.com>
2017-01-14 19:25:44 +0000

    Added master layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('master')
+@section('title', 'Home')
 
-        <title>Home Page</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-
-        <script src="//code.jquery.com/jquery-3.1.0.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Home Page
-                </div>
-
-                <div class="quote">
-                    Our Home page.
-                </div>
-            </div>
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">Home Page</div>
+            <div class="quote">Our Home page!</div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
